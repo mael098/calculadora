6f27491f2c41e5f72a2996949c847af0164f3798
mael098 <juanvillegasmorales@outlook.com>
2023-07-20 17:41:08 -0600

    remove file from project and add styles and file

---
+++ src/components/calculadora.jsx
@@ -0,0 +1,14 @@
+import './calculadora.css'
+//esla inportacion de estilos
+
+
+const calcualadoraBonita = () => {
+  return (
+    <main> 
+    <div>
+      <h1>Calculadora</h1>
+    </div>
+    </main>
+  )
+}
+export default calcualadoraBonita
